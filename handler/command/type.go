405359f3929aef0752968f2f8b0d73f8d27c2409
isso0424 <isso04240129@gmail.com>
2021-03-03 01:56:02 +0900

    feat: define command interface

---
+++ handler/command/type.go
@@ -0,0 +1,9 @@
+package command
+
+import "isso0424/dise/types"
+
+type Command interface {
+	GetPrefix() string
+	ValidateArgs(args []string) bool
+	Exec(channelID string, args []string, session types.Session)
+}
